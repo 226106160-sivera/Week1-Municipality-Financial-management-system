e5d9680477186655592eccdc549e1c31cb856742
226106160-sivera <andriasdrezzy@gmail.com>
2026-08-26 16:38:43 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+  char municipality[50];
+  char mayor[50];
+  int population;
+
+  printf(" Municipality Financial Management system\n\n");
+
+printf("Enter municipality name: ");
+scanf("%49s",& municipality);
+
+printf("Enter Mayor: "); 
+scanf("%49s",& mayor);
+printf("Enter Population: "); 
+scanf("%d",& population);
+printf("\n---------------------------------\n"); 
+printf("Municipality: %s\n", municipality); 
+printf("Mayor       : %s\n", mayor); 
+printf("Population  : %d\n", population); 
+
+return 0;
+}
